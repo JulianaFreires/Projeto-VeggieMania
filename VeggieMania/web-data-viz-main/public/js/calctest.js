5d8827bf722e9e33875c72d8fc4f9d922c0e28f8
JulianaFreires <juliana.silva@sptech.school>
2024-06-27 22:24:23 -0300

    27/06 - Ajuste na calculadora HTML + JS - Adicionando novas informações nutricionais (carboidratos, fibras alimentares e lipideos) + Novos alimentos + Atualização das informações nutricionais de cada alimento

--- VeggieMania/web-data-viz-main/public/js/calctest.js
+++ VeggieMania/web-data-viz-main/public/js/calctest.js
@@ -1,17 +1,26 @@
 
 //24/06 Mais aliementos foram adicionados e estão relacionados a 3 categorias: massas, laticínios e tubérculos.
-//25/06 Os queijos foram para as listas dos laticínios
-var nome = ["Aveia em flocos", "Quinoa em grãos", "Milho", "Arroz integral", "Arroz Branco", "Ervilha", "Feijão branco", "Lentilha", "Grão-de-bico", "Feijão preto", "Feijão carioca", "Tofu (com sulfato de cálcio)", "Amêndoa", "Castanha de caju", "Amendoim", "Noz pecã", "Gergelim", "Girassol", "Brócolis", "Agrião", "Mostarda", "Rúcula", "Couve", "Alface lisa", "Escarola", "Palmito", "Abobrinha", "Berinjela", "Cenoura", "Chuchu", "Sardinhas", "Pescada", "Coxão mole bovino (sem gordura, cozido)", "Músculo bovino (sem gordura, cozido)", "Contra-filé bovino (sem gordura, cru)", "Peito de frango (sem osso, cru)", "Coxa de frango (sem osso, crua)", "Macarrão", "Pão francês", "Pão puma ", "Pão puma integral", "Pão puma de milho", "Requeijão", "Cream cheese", "Manteiga", "Margarina", "Doce de leite", "Maionese", "Nutella", "Pasta de amendoim", "Queijo Prato", "Queijo Mozzarella", "Ricota", "Mandioca", "Batata inglesa", "Batata doce", "Mandioquinha", "Inhame", "Rabanete", "Beterraba"];
+//27/06 Os queijos foram para as listas dos laticínios
+//27/06 Foram adicionados 25 alimentos, além de novas informações nutricionais que incluem carboidrato, fibra alimentar e lipideos. As informações dos nutrientes foram alteradas considerando a sua forma de preparo como cozidos com exceção de algumas carnes e folhas.
 
-var caloria = [3.94, 3.74, 3.65, 3.59, 1.28, 3.41, 3.33, 3.47, 3.87, 3.31, 3.36, 0.76, 5.78, 5.74, 5.67, 6.91, 5.67, 5.70, 0.25, 0.17, 0.26, 0.25, 0.30, 0.14, 0.23, 0.285, 0.19, 0.20, 0.34, 0.17, 1.14, 1.11, 2.19, 1.94, 1.57, 1.18, 1.20, 3.71,3.00,2.53,2.53,5.45,2.81,2.32,7.26,7.23,3.35,3.0833,5.42,6.0667, 3.52, 3.52, 1.74, 1.25,0.52,0.77,1.32,0.97,0.14,0.32];
+var nome = ["Aveia em Flocos", "Quinoa em Grãos", "Milho Verde em lata", "Arroz Integral", "Arroz Branco", "Ervilha em lata", "Feijão Branco", "Lentilha", "Grão-de-Bico", "Feijão Preto", "Feijão Carioca", "Tofu", "Amêndoa torrada", "Castanha de caju", "Amendoim torrado", "Noz pecã", "Gergelim", "Pasta de amendoim", "Brócolis", "Agrião", "Mostarda folha", "Rúcula", "Couve manteiga refogada", "Alface lisa", "Espinafre (refogado)", "Couve-flor", "Escarola", "Palmito pupunha", "Abobrinha", "Berinjela", "Cenoura", "Abóbora cabotiá", "Chuchu", "Sardinha em lata", "Pescada", "Coxão mole bovino (sem gordura, cozido)", "Músculo bovino (sem gordura, cozido)", "Contra-filé bovino (sem gordura, grelhado)", "Peito de frango (sem osso, assado)", "Coxa de frango (com pele, assado)", "Carne bovina lagarto (cozido)", "Filé mignon bovino (sem gordura, grelhado)", "Carne bovina, patinho, sem gordura (grelhado)", "Salmão sem pele (grelhado)", "Ovo", "Macarrão", "Pão francês", "Pão puma", "Pão puma integral", "Pão puma de milho", "Requeijão", "Cream cheese", "Manteiga", "Margarina", "Doce de leite", "Maionese tradicional", "Queijo ricota", "Queijo muçarela", "Queijo prato", "Queijo parmesão","Mandioca", "Batata inglesa", "Batata doce", "Mandioquinha", "Inhame", "Rabanete", "Beterraba"]; 
 
-var proteina = [0.14, 0.131, 0.0942, 0.080, 0.025, 0.2454, 0.2336, 0.23, 0.2238, 0.21, 0.20, 0.08, 0.2126, 0.1531, 0.257, 0.0917, 0.1829, 0.2278, 0.04, 0.03, 0.027, 0.0258, 0.0245, 0.02, 0.017, 0.028, 0.01, 0.01, 0.01, 0.01, 0.21, 0.16, 0.32, 0.31, 0.24, 0.22, 0.18, 0.10,0.08,0.12,0.094,0.203,0.099,0.06,0.004,0.00,0.075,0.075,0.063,0.2733, 0.2378, 0.2378, 0.1126, 0.006,0.012,0.006,0.021,0.021,0.014,0.013];
+var caloria = [3.94, 1.14, 0.98, 1.24, 1.28, 0.74, 1.29, 0.93, 1.15, 0.77, 0.76, 0.64, 6.32, 6.43, 6.06, 7.42, 5.84, 6.15, 0.25, 0.17, 0.18, 0.13, 90, 0.14, 0.67, 0.19, 0.33, 0.29, 0.15, 0.19, 0.30, 0.48, 0.19, 2.85, 2.23, 2.19, 1.94, 1.94, 1.59, 2.15, 2.22, 2.20, 2.19, 2.43, 1.43, 3.71, 3, 2.92, 2.53, 2.92, 2.57, 2.35, 7.26, 7.23, 3.06, 3.02, 1.40, 3.30, 3.60, 4.53,1.25, 0.52, 0.77, 0.80, 0.89, 0.14, 0.32]; 
 
-var ferro = [0.0472, 0.0925, 0.0271,0.001, 0.01, 0.0443, 0.1044, 0.07, 0.0686, 0.065, 0.08, 0.0536, 0.043, 0.060, 0.0458, 0.0253, 0.0778, 0.0673, 0.006, 0.031, 0.014, 0.0146, 0.0019, 0.006, 0.009, 0.0313, 0.002, 0.002, 0.002, 0.002, 0.013, 0.002, 0.026, 0.024, 0.024, 0.004, 0.008, 0.009,0.01,0.057,0.03,0.087,0.0013,0.0086,0.002,0.001,1.95,0.00,0.00,0.00,0.0043, 0.0044, 0.0038,0.001,0.002,0.002,0.006,0.004,0.004,0.002];
+var carboidrato = [0.67, 0.213, 0.17, 0.26, 0.28, 0.13, 0.25, 0.16, 0.208, 0.14, 0.14, 0.02, 0.215, 0.15, 0.19, 0.136, 0.22, 0.151, 0.04, 0.02, 0.03, 0.02, 8.70, 0.02, 0.04, 0.04, 0.0162, 0.06, 0.03, 0.04, 0.07, 0.11, 0.05, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.02, 0.78, 0.59, 0.56, 0.5, 0.56, 0.02, 0.0224, 0, 0, 0.59, 0.08, 0.04, 0.03, 0.02, 0.02, 0.30, 0.12, 0.18, 0.19, 0.189, 0.03, 0.07]; 
 
-var calcio = [0.54, 0.60, 0.07, 0.07,0.04, 0.55, 2.40, 0.54, 1.05, 1.11, 1.23, 3.50, 2.48, 0.45, 0.92, 0.70, 1.31, 1.16, 0.86, 1.33, 1.03, 1.60, 1.45, 0.28, 1.00, 0.58, 0.15, 0.09, 0.23, 0.12, 1.67, 0.14, 0.04, 0.05, 0.04, 0.07, 0.08, 0.17,0.16,1.56,1.32,2.34,2.83,0.539,0.09,0.03,2.45,0.00,0.00,0.00, 6.50, 5.05, 2.07, 0.19,0.04,0.17,0.16,0.12,0.21,0.15];
+var lipideo = [0.09, 0.0192, 0.02, 0.01, 0, 0, 0.0035, 0.01, 0.0186, 0.01, 0.01, 0.04, 0.499, 0.64, 0.54, 0.743, 0.5, 0.484, 0.01, 0, 0, 0, 6.60, 0, 0.05, 0, 0.0155, 0.01, 0, 0, 0, 0.01, 0, 0.24, 0.12, 0.09, 0.07, 0.04, 0.03, 0.10, 0.09, 0.09, 0.07, 0.14, 0.09, 0.01, 0.03, 0.03, 0.04, 0.03, 0.23, 0.225, 0.82, 0.82, 0.06, 0.30, 0.08, 0.25, 0.29, 0.34, 0, 0, 0, 0, 0.0014, 0, 0];
 
-var zinco = [0.0397, 0.033, 0.0221, 0.013, 0.005, 0.0301, 0.0367, 0.035, 0.0281, 0.029, 0.029, 0.008, 0.0336, 0.056, 0.0327, 0.0453, 0.1023, 0.0506, 0.005, 0.007, 0.0022, 0.0047, 0.0013, 0.003, 0.0042, 0.0115, 0.002, 0.001, 0.002, 0.001, 0.013, 0.003, 0.047, 0.064, 0.032, 0.007, 0.022, 0.008, 0.008, 0.013, 0.016, 0.021, 0.014, 0.0039, 0.0, 0.0, 0.005, 0.0, 0.0, 0.032,0.026, 0.0292, 0.0116, 0.002, 0.002, 0.001, 0.006, 0.003, 0.002, 0.004];
+ var fibraAlimentar = [0.09, 0.028, 0.05, 0.03, 0.02, 0.05, 0.0627, 0.08, 0.0747, 0.08, 0.09, 0.01, 0.125, 0.08, 0.08, 0.094, 0.12, 0, 0.03, 0.02, 0.02, 0.02, 5.70, 0.02, 0.03, 0.02, 0.0227, 0.03, 0.02, 0.03, 0.03, 0.03, 0.01, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.03, 0.02, 0.04, 0.07, 0.04, 0,0,0,0,0,0,0,0,0,0, 0.02, 0.01, 0.02, 0.02, 0.016, 0.02, 0.02]; 
+
+var proteina = [0.14, 0.044, 0.03, 0.03, 0.03, 0.05, 0.0969, 0.06, 0.0753, 0.04, 0.05, 0.07, 0.212, 0.14, 0.23, 0.095, 0.21, 0.265, 0.02, 0.03, 0.02, 0.02, 1.70, 0.02, 0.03, 0.01, 0.0192, 0.03, 0.01, 0.01, 0.01, 0.01, 0, 0.16, 0.27, 0.32, 0.31, 0.36, 0.32, 0.28, 0.33, 0.33, 0.36, 0.26, 0.13, 0.1, 0.08, 0.08, 0.09, 0.08, 0.10, 0.06, 0, 0, 0.06, 0.01, 0.13, 0.23, 0.23, 0.36, 0.01, 0.01, 0.01, 0.01, 0.023, 0.01, 0.01]; 
+
+var ferro = [0.04, 0.0149, 0.01, 0, 0, 0.01, 0.0369, 0.01, 0.0243, 0.01, 0.01, 0.01, 0.0371, 0.02, 0.01, 0.028, 0.05, 0.0327, 0.01, 0.03, 0.01, 0.01, 0.50, 0.01, 0.01, 0, 0.0088, 0, 0, 0, 0, 0, 0, 0.04, 0.01, 0.03, 0.02, 0.02, 0, 0.01, 0.02, 0.03, 0.03, 0, 0.02, 0.01, 0.01, 0.03, 0.03, 0.03, 0, 0.0087, 0, 0, 0, 0, 0, 0, 0, 0.01, 0, 0, 0, 0, 0.0002, 0, 0]; 
+
+var calcio = [0.48, 0.17, 0.02, 0.05, 0.04, 0.22, 0.897, 0.16, 0.287, 0.29, 0.27, 0.81, 2.69, 1.46, 0.39, 0.72, 8.25, 0, 0.51, 1.33, 0.68, 1.17, 177, 0.28, 1.12, 0.16, 0.0091, 0.32, 0.17, 0.11, 0.26, 0.08, 0.08, 5.50, 3.78, 0.04, 0.05, 0.05, 0.05, 0.08, 0.04, 0.04, 0.05, 0.15, 0.42, 0.17, 0.16, 0.78, 1.32, 0.78, 2.59, 0.547, 0.09, 0.03, 1.95, 0.03, 2.53, 8.75, 9.40, 9.92, 0.19, 0.04, 0.17, 0.12, 0.149, 0.21, 0.15];
+
+ var zinco = [0.03, 0.0109, 0.01, 0.01, 0.01, 0.01, 0.0265, 0.01, 0.0205, 0.01, 0.01, 0.01, 0.0312, 0.04, 0.02, 0.0507, 0.05, 0.0305, 0, 0.01, 0, 0, 0.20, 0, 0.01, 0, 0.0083, 0, 0, 0, 0, 0, 0, 0.02, 0.01, 0.05, 0.06, 0.05, 0.01, 0.03, 0.07, 0.04, 0.08, 0.01, 0.01, 0.01, 0.01, 0.01, 0.02, 0.01, 0.01, 0.0039, 0, 0, 0.01, 0, 0.01, 0.04, 0.04, 0.04, 0, 0, 0, 0, 0.0036, 0, 0];
+
 
 window.onload = function () { // Executa quando a janela é carregada
     let checkboxes = document.querySelectorAll('.caixa');  // Seleciona todas as checkboxes com a classe 'caixa'
@@ -50,6 +59,9 @@ function calcular() {
     var selecionados = []; // Array para armazenar os ids das checkboxes selecionadas
     var texto = ""
     var totalCalorias = 0;
+    var totalCarboidratos = 0; // 27/06
+    var totalLipideos = 0; // 27/06
+    var totalFibras = 0; // 27/06
     var totalProteinas = 0;
     var totalFerro = 0;
     var totalCalcio = 0;
@@ -69,6 +81,9 @@ function calcular() {
                 // Calcula os valores nutricionais multiplicando pela quantidade
                 nomel = nome[checkboxes[i].id]
                 calorial = Number(caloria[checkboxes[i].id] * quantidade).toFixed(2);
+                carboidratol = Number(carboidrato[checkboxes[i].id] * quantidade).toFixed(2);// 27/06
+                lipideosl = Number(lipideo[checkboxes[i].id] * quantidade).toFixed(2);// 27/06
+                fibral = Number(fibraAlimentar[checkboxes[i].id] * quantidade).toFixed(2);// 27/06
                 proteinal = Number(proteina[checkboxes[i].id] * quantidade).toFixed(2);
                 ferrol = Number(ferro[checkboxes[i].id] * quantidade).toFixed(2);
                 calciol = Number(calcio[checkboxes[i].id] * quantidade).toFixed(2);
@@ -76,6 +91,9 @@ function calcular() {
 
 
                 totalCalorias += Number(calorial);
+                totalCarboidratos += Number(carboidratol);// 27/06
+                totalLipideos += Number(lipideosl);// 27/06
+                totalFibras += Number(fibral)// 27/06
                 totalProteinas += Number(proteinal);
                 totalFerro += Number(ferrol);
                 totalCalcio += Number(calciol);
@@ -85,7 +103,7 @@ function calcular() {
                 document.querySelector('.resposta').style.display = 'flex';
                 document.getElementById('resul').scrollIntoView({ behavior: 'smooth' });
 
-                texto += `<span>${nomel} - Calorias: ${calorial} | Proteinas: ${proteinal} | Cálcio: ${calciol} | Ferro: ${ferrol} | Zinco: ${zincol}<br></span>`
+                texto += `<span>${nomel} - Calorias: ${calorial} |  Carboidratos:: ${carboidratol} |  Lipideos: ${lipideosl} |  Fibra alimentar: ${fibral} | Proteinas: ${proteinal} | Cálcio: ${calciol} | Ferro: ${ferrol} | Zinco: ${zincol}<br></span>`// 27/06
 
             } else {
                 alert(`${nome[checkboxes[i].id]} - Selecione um valor válido na gramatura`)
@@ -99,11 +117,14 @@ function calcular() {
     }
 
     totalCaloriasFixed = Number(totalCalorias).toFixed(2);
+    totalCarboidratosFixed = Number(totalCarboidratos).toFixed(2);// 27/06
+    totalLipideosFixed = Number(totalLipideos).toFixed(2);// 27/06
+    totalFibrasFixed = Number(totalFibras).toFixed(2);// 27/06
     totalProteinasFixed = Number(totalProteinas).toFixed(2);
     totalFerroFixed = Number(totalFerro).toFixed(2);
     totalCalcioFixed = Number(totalCalcio).toFixed(2);
     totalZincoFixed = Number(totalZinco).toFixed(2);
 
 
-    div_mensagem.innerHTML += `<p>${texto}<br> O prato contém no total: Calorias: ${totalCaloriasFixed} | Proteina: ${totalProteinasFixed} | Cálcio: ${totalCalcioFixed} | Ferro: ${totalFerroFixed} | Zinco:${totalZincoFixed}</p>`
+    div_mensagem.innerHTML += `<p>${texto}<br> O prato contém no total: Calorias: ${totalCaloriasFixed} | Carboidratos: ${totalCarboidratosFixed} | Lipideos: ${totalLipideosFixed} | Fibras alimentares: ${totalFibrasFixed} | Proteina: ${totalProteinasFixed} | Cálcio: ${totalCalcioFixed} | Ferro: ${totalFerroFixed} | Zinco:${totalZincoFixed}</p>`// 27/06
 }
